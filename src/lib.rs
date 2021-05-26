eee64248e8b3a7ed44447af3dc064eb4ad7073b7
5apristi <doucet.natanael@protonmail.com>
2021-05-26 15:45:02 +0200

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,23 @@ pub mod text_buffer;
 
 /* re-exporting Editor to make it visible from main.rs */
 pub use editor::Editor;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_editor() {
+        let _editor = editor::Editor::new(Option::None);
+    }
+
+    #[test]
+    fn init_text_buffer() {
+        let _buffer = text_buffer::Buffer::new_empty();
+    }
+
+    #[test]
+    fn init_terminal() {
+        let _terminal = terminal::Terminal::new();
+    }
+}
